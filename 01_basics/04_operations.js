6a0e64492cfdc21fa18f0bd7b994e5b6dc103183
niks4920 <niks4920@gmail.com>
2025-03-02 14:14:56 +0000

    new changes

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value
+//console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)  // 2 raise to power 3
+//console.log(2/2)
+//console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Nikky"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
