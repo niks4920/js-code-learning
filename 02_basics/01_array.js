3d8a5420490716c89187cbb1b3610d112a796093
niks4920 <niks4920@gmail.com>
2025-04-27 12:51:34 +0000

    add

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,5 +1,6 @@
 // Array declaration
 const myArray = [0,1,2,3,4,true,"niketa"]
+const myArray2 = ["shaktiman","naagraj"]
 const myArr = [0,1,2,3,4,5]
 console.log(myArr[0])
 const myArr2 = new Array(1,2,3)
